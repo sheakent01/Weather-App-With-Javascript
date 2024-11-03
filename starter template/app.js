8897c79c02cb90311a978bb6b2b302e3a65634e3
sheakent01 <154026680+sheakent01@users.noreply.github.com>
2024-11-03 01:24:16 +0000

    Build Cont.

--- starter template/app.js
+++ starter template/app.js
@@ -30,5 +30,51 @@ descElement.innerHTML = Clear sky
 
 locationElement.innerHTML = London, GB 
      ${weather.city}, ${weather.country};
-     
+
     }
+
+function celsiusToFahrenheit(temperature){
+    (temperature * 9/5) + 32;
+}
+
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    if(weather.temperature.unit === "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        tempElement.innerHTML = ${fahrenheit}° <span>F</span>;
+        weather.temperature.unit = "fahrenheit";
+
+    }else{
+   
+        tempElement.innerHTML = ${weather.temperature.value}° <span>C</span>;
+    }
+}
+});
+
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser Doesn't Support Geolocation.</p>"
+}
+
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude.longtidue);
+}
+
+function showError(error) {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML=<p> ${error.message} </p>;
+}
+
+const KELVIN = 273;
+
+const key = "";
+
+function getWeather(latitude, longitude){
+    let api = 
+    fetch(api)
+}
