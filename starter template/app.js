ccf52cab0f17b22553fafe2f3e918bc020efc863
sheakent01 <154026680+sheakent01@users.noreply.github.com>
2024-11-03 01:58:36 +0000

    Build. Cont.

--- starter template/app.js
+++ starter template/app.js
@@ -82,6 +82,7 @@ const key = "82005d27a116c2880c8f0fcb866998a0";
 
 //Get weather from API provider
 function getWeather(latitude, longitude){
-    let api = "http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}"
+    let api = "http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}";
     fetch(api)
+    .then(function(response))
 }
